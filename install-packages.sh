b44517a288cc75b1539b789b5437d251972b1350
Henry Snoek <henry.snoek@schiphol.nl>
2017-12-29 17:21:33 +0100

    add docker compose install

--- install-packages.sh
+++ install-packages.sh
@@ -2,13 +2,17 @@
 
 # apt packages
 sudo apt update
-sudo apt install python3-dev python3-pip git make wget
+sudo apt install python3-dev python3-pip git make wget vim
 
 # python packages
 sudo pip3 install thefuck
 
+# perl packages
+sudo cpan Crypt::HSXKPasswd
+
 # git repos
-wget https://github.com/bbusschots/hsxkpasswd/releases/download/v3.6/Crypt-HSXKPasswd-v3.6.tar.gz
-cd ~
 git clone https://github.com/magicmonty/bash-git-prompt.git .bash-git-prompt --depth=1
 
+# docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
