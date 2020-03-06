a0777cebb9f3150bf7ec38336cdabad2bbfd9fbd
Henry Snoek <henry@overstappen.nl>
2020-03-06 17:32:48 +0100

    disable bluetooth on startup

--- configure.sh
+++ configure.sh
@@ -2,3 +2,10 @@
 
 # force alt + tab to switch only on current workspace
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
+
+# disable bluetooth
+sudo install -b -m 755 /dev/stdin /etc/rc.local << EOF
+#!/bin/sh
+rfkill block bluetooth
+exit 0
+EOF
